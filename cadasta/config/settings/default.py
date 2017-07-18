e1d6402d69d0763993d589410d7d42dc65b17ee4
Anthony Lukach <alukach@cadasta.org>
2017-07-18 13:57:58 -0600

    Add backend-tooling for asynchronous tasks (#1624)

--- cadasta/config/settings/default.py
+++ cadasta/config/settings/default.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = (
     'party',
     'xforms',
     'search',
+    'tasks',
 
     'crispy_forms',
     'parsley',
@@ -568,3 +569,5 @@ ES_SCHEME = 'http'
 ES_HOST = 'localhost'
 ES_PORT = '9200'
 ES_MAX_RESULTS = 10000
+
+CELERY_QUEUES = ['export']
